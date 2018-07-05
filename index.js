a9e787736fe11ca26be5de82cff481d9293e9d56
Ola Assem <o.assem@gmail.com>
2018-07-05 12:24:44 +0200

    projects section images

--- index.js
+++ index.js
@@ -1,15 +1,70 @@
 //Change nav background color on scroll
-let offset = $('main').offset().top;
+let offset = $('#white').offset().top;
+let green = $('#green').offset().top;
+let yellow = $('#yellow').offset().top;
+let red = $('#red').offset().top;
 let navbar = $('.navigation-container');
 let navlinks = $('.nav-links, .navigation-container a');
 
 $(document).scroll(function() {
-    position = $(this).scrollTop();
-    if (position < offset){
-        navbar.css({'background-color':'#0032D3','color':'white' });
-        navlinks.css('color','white');
-    }else{
-        navbar.css({'background-color':'#E6EEFF', 'color':'#0032D3' });
-        navlinks.css('color','#0032D3');
-    }
+  position = $(this).scrollTop();
+  if (position < offset) {
+    navbar.css({
+      'background-color': '#0032D3',
+      'color': 'white',
+      'transition-property': 'background-color',
+      'transition-duration': '0.4s',
+      'transition-timing-function': 'linear'
+    });
+    navlinks.css('color', 'white');
+  }
+  if (position > green && position < yellow) {
+    navbar.css({
+      'background-color': '#3FEFE6',
+      'color': '#282C34',
+      'transition-property': 'background-color',
+      'transition-duration': '0.4s',
+      'transition-timing-function': 'linear'
+    });
+    navlinks.css('color', '#282C34');
+  }
+  if (position > yellow && position < red) {
+    navbar.css({
+      'background-color': '#D2F97A',
+      'color': '#282C34',
+      'transition-property': 'background-color',
+      'transition-duration': '0.4s',
+      'transition-timing-function': 'linear'
+    });
+    navlinks.css('color', '#282C34');
+  }
+  if (position > offset && position < green) {
+    navbar.css({
+      'background-color': '#E6EEFF',
+      'color': '#0032D3',
+      'transition-property': 'background-color',
+      'transition-duration': '0.4s',
+      'transition-timing-function': 'linear'
+    });
+    navlinks.css('color', '#0032D3');
+  }
+  if (position > red) {
+    navbar.css({
+      'background-color': '#fa5f5f',
+      'color': 'white',
+      'transition-property': 'background-color',
+      'transition-duration': '0.4s',
+      'transition-timing-function': 'linear'
+    });
+    navlinks.css('color', 'white');
+  }
+});
+
+
+//scrolling on link click functionality
+$('.name').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 2000);
+    return false;
 });
